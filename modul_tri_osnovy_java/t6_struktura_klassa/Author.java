e3756237fe7f66af4a644aef61e3a94ef4ec081f
Danil Ponomarenko <el.pochtadp@gmail.com>
2026-03-16 11:30:45 +0300

    Create Author.java

---
+++ modul_tri_osnovy_java/t6_struktura_klassa/Author.java
@@ -0,0 +1,11 @@
+public class Author {
+    String name;
+    String surname;
+    int rating;
+
+    public Author(String name, String surname, int rating) {
+        this.name = name;
+        this.surname = surname;
+        this.rating = rating;
+    }
+}
