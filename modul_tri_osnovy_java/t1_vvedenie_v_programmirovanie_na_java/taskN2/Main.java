d0f1805b1f2a02669f90b98d4d389be5c48adcfb
Danil Ponomarenko <el.pochtadp@gmail.com>
2026-03-16 11:43:27 +0300

    Create Main.java

---
+++ modul_tri_osnovy_java/t1_vvedenie_v_programmirovanie_na_java/taskN2/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int income = 44000;
+        int spending = 30000;
+
+        int balance = income - spending;
+
+        System.out.println("Итого (руб):\n" + balance);
+    }
+}
